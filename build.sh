077bd67a1f9c3bdd00ecc0cc3cace41895fd847a
Raleigh-Foster <ralsusbrad@aol.com>
2016-04-10 20:22:02 -0700

    updated

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
+cd Back-End
+cd Glue
+make
+cd ../..
+cd Front-End
+cd UrWeb
+urweb app
+cd ../..
+cd Back-End
+cd Glue
+gcc -g -O -c reader.c
+cd ..
+cd Idris
 idris --log 1 --build elementalmagicks.ipkg
