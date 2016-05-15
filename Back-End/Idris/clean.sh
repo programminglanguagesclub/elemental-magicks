6db9e17a96502aaf481ea9345b44d83b9a4c5087
Raleigh-Foster <ralsusbrad@aol.com>
2016-05-15 00:38:07 -0700

    added trash file removal to clean script

--- Back-End/Idris/clean.sh
+++ Back-End/Idris/clean.sh
@@ -11,3 +11,18 @@ rm step_game.ibc
 rm step_game_helpers.ibc
 rm transform_game.ibc
 rm transform_game_helpers.ibc
+
+rm clean.sh~
+rm clientupdates.idr~
+rm elementalmagicks.ipkg~
+rm main.idr~
+rm objects_advanced.idr~
+rm objects_basic.idr~
+rm phase.idr~
+rm preliminaries.idr~
+rm serverupdates.idr~
+rm skill_dsl.idr~
+rm step_game.idr~
+rm step_game_helpers.idr~
+rm transform_game.idr~
+rm transform_game_helpers.idr~
