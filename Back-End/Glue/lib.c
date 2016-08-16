8fbaab6b49770f1a93706503da380c6d37277e45
Raleigh-Foster <ralsusbrad@aol.com>
2016-08-15 19:21:54 -0700

    Update lib.c

--- Back-End/Glue/lib.c
+++ Back-End/Glue/lib.c
@@ -2,27 +2,13 @@
 #include <stdio.h>
 #include <urweb.h>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+
 #define MAX_BUF 1024
-
-
 
 
 char *reader()
@@ -33,26 +19,45 @@ char *reader()
         char * myfifo = "/tmp/myfifo";
 	
 	//Set to empty
-	char *buf;
-	buf = (char *) malloc(1024); // memory leak
-	strcpy(buf, "empty");
+	char *sizeOfIncommingMessage;
+	sizeOfIncommingMessage = (char *) malloc(1024); // memory leak
+	strcpy(sizeOfIncommingMessage, "empty");
         //char buf[MAX_BUF] = "empty";
 
         // open, read, and display the message from FIFO 
 	//Keep pipe open untill it gets a message
-	while(0 == strcmp("empty", buf))
+	while(0 == strcmp("empty", sizeOfIncommingMessage))
 	{
 		//Check in FIFO folder for a open pipe
         	fd = open(myfifo, O_RDONLY);
 		
 		//Read the pipe (location, message, size of message)
-        	read(fd, buf, MAX_BUF);
+        	read(fd, sizeOfIncommingMessage, MAX_BUF);
+	}
+	
+	//Set to empty
+	char *message;
+	message = (char *) malloc(1024); // memory leak
+	strcpy(message, "empty");
+
+        // open, read, and display the message from FIFO 
+	//Keep pipe open untill it gets a message
+	while(0 == strcmp("empty", message))
+	{
+		//Check in FIFO folder for a open pipe
+        	fd = open(myfifo, O_RDONLY);
+		
+		//Read the pipe (location, message, size of message)
+        	read(fd, message, MAX_BUF);
 	}
 	//Print the message
-	 printf("Received: %s\n", buf);
+	 printf("Received: %s\n", sizeOfIncommingMessage);
+
+	//Print the message
+	 printf("Received: %s\n", message);
 
         close(fd);
-	return buf;
+	return message;
 }
 
 void writer(char *message)
@@ -76,20 +81,6 @@ int fd;
         //remove the FIFO 
         unlink(myfifo);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
