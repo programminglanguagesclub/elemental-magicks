adc3acff8fe964a0583afb5e8c3d57553e13f509
Raleigh-Foster <foster@cs.uoregon.edu>
2016-10-03 01:19:23 -0700

    added .idr extension

--- Back-End/Idris/cards/spells/touch.sh
+++ Back-End/Idris/cards/spells/touch.sh
@@ -2,10 +2,10 @@
 
 for i in 1 2 3 4 5 6 7 8 9
 do
-  touch earth_$i
-  touch fire_$i
-  touch water_$i
-  touch air_$i
-  touch spirit_$i
-  touch void_$i
+  touch earth_$i.idr
+  touch fire_$i.idr
+  touch water_$i.idr
+  touch air_$i.idr
+  touch spirit_$i.idr
+  touch void_$i.idr
 done
