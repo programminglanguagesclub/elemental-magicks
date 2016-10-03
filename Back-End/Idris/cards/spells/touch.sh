68d726da5918a9a442428443f6812af5eac7deaf
Raleigh-Foster <foster@cs.uoregon.edu>
2016-10-03 00:29:23 -0700

    added some files

---
+++ Back-End/Idris/cards/spells/touch.sh
@@ -0,0 +1,11 @@
+
+
+for i in 1 2 3 4 5 6 7 8 9
+do
+  touch earth_$i
+  touch fire_$i
+  touch water_$i
+  touch air_$i
+  touch spirit_$i
+  touch void_$i
+done
---
+++ Back-End/Idris/cards/units/touch.sh
@@ -0,0 +1,27 @@
+
+
+for i in 1 2 3 4 5 6 7 8 9
+do
+  touch no_schools_$i
+  touch earth_$i
+  touch fire_$i
+  touch water_$i
+  touch air_$i
+  touch spirit_$i
+  touch void_$i
+  touch earth_fire_$i
+  touch earth_water_$i
+  touch earth_air_$i
+  touch earth_spirit_$i
+  touch earth_void_$i
+  touch fire_water_$i
+  touch fire_air_$i
+  touch fire_spirit_$i
+  touch fire_void_$i
+  touch water_air_$i
+  touch water_spirit_$i
+  touch water_void_$i
+  touch air_spirit_$i
+  touch air_void_$i
+  touch spirit_void_$i
+done
